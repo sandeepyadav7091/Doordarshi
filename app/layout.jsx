02392660111cd7f0cd56198c5a60cbb9d0a188aa
sandeepyadav7091 <sandeepkryadav@reospark.com>
2026-08-13 13:06:58 +0530

    yuyuy

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,8 +12,8 @@ export const metadata = {
   keywords: ["news", "breaking news", "india news"],
   metadataBase: new URL("https://doordarshisamachar.in"),
   icons: {
-    icon: "https://doordarshisamachar.in/icon.png",
-    shortcut: "https://doordarshisamachar.in/icon.png",
+    icon: "https://akm-img-a-in.tosshub.com/aajtak/images/story/202608/6a7d53dcbe5f5-cab-booking-tip-rules-131919136-16x9.jpg?size=948:533",
+    shortcut: "https://akm-img-a-in.tosshub.com/aajtak/images/story/202608/6a7d53dcbe5f5-cab-booking-tip-rules-131919136-16x9.jpg?size=948:533",
   },
 
   openGraph: {
@@ -23,7 +23,7 @@ export const metadata = {
     siteName: "Doordarshi Samachar",
     images: [
       {
-        url: "https://doordarshisamachar.in/logo.png",
+        url: "https://akm-img-a-in.tosshub.com/aajtak/images/story/202608/6a7d53dcbe5f5-cab-booking-tip-rules-131919136-16x9.jpg?size=948:533",
         width: 1200,
         height: 630,
       },
