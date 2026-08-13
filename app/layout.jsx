5cdafb4f76e3c88d46cf093cf76ae75e79bff926
sandeepyadav7091 <sandeepkryadav@reospark.com>
2026-08-13 15:41:56 +0530

    jhbhj

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,20 +16,20 @@ export const metadata = {
     shortcut: "https://doordarshisamachar.in/logo.png",
   },
 
-  openGraph: {
-    title: "Doordarshi Samachar",
-    description: "Latest breaking news",
-    url: "https://doordarshisamachar.in",
-    siteName: "Doordarshi Samachar",
-    images: [
-      {
-        url: "https://doordarshisamachar.in/logo.png",
-        width: 1200,
-        height: 630,
-      },
-    ],
-    type: "website",
-  },
+  // openGraph: {
+  //   title: "Doordarshi Samachar",
+  //   description: "Latest breaking news",
+  //   url: "https://doordarshisamachar.in",
+  //   siteName: "Doordarshi Samachar",
+  //   images: [
+  //     {
+  //       url: "https://doordarshisamachar.in/logo.png",
+  //       width: 1200,
+  //       height: 630,
+  //     },
+    // ],
+    // type: "website",
+  // },
 };
 
 export default function RootLayout({ children }) {
