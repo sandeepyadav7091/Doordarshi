bfe66b9fc3cd04889881c38e8a0b94aac4525dc7
sandeepyadav7091 <sandeepkryadav@reospark.com>
2026-08-13 16:44:12 +0530

    jhbhj

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,13 +21,13 @@ export const metadata = {
     description: "Latest breaking news",
     url: "https://doordarshisamachar.in",
     siteName: "Doordarshi Samachar",
-    images: [
-      {
-        url: "https://akm-img-a-in.tosshub.com/aajtak/images/story/202608/6a7d53dcbe5f5-cab-booking-tip-rules-131919136-16x9.jpg?size=948:533",
-        width: 1200,
-        height: 630,
-      },
-    ],
+    // images: [
+    //   {
+    //     url: "https://akm-img-a-in.tosshub.com/aajtak/images/story/202608/6a7d53dcbe5f5-cab-booking-tip-rules-131919136-16x9.jpg?size=948:533",
+    //     width: 1200,
+    //     height: 630,
+    //   },
+    // ],
     type: "website",
   },
 };
